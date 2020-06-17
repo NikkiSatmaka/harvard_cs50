08e5ea0729e502ca44ce8292d85601d85bc30055
Nikki Satmaka <dukemon.kh@gmail.com>
2020-06-18 00:30:23 +0700

    Finish pset4/filter/grayscale

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -3,6 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
+        }
+    }
     return;
 }
 
