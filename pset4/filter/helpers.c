4ed4e8e039a7a39ddd93a0fe761a3e38548a5109
Nikki Satmaka <dukemon.kh@gmail.com>
2020-06-18 17:29:37 +0700

    add reverse filter

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -8,6 +8,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             BYTE avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            if ((int) avg > 255)
+            {
+                avg = 255;
+            }
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
@@ -19,6 +23,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0, half_width = width / 2; j < half_width; j++)
+        {
+            BYTE tmpBlue = image[i][j].rgbtBlue;
+            BYTE tmpGreen = image[i][j].rgbtGreen;
+            BYTE tmpRed = image[i][j].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width - 1 - j].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width - 1 - j].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width - 1 - j].rgbtRed;
+            image[i][width - 1 - j].rgbtBlue = tmpBlue;
+            image[i][width - 1 - j].rgbtGreen = tmpGreen;
+            image[i][width - 1 - j].rgbtRed = tmpRed;
+        }
+    }
     return;
 }
 
